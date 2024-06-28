44a5aa9ad0876478fd7405770f2eddf8dd54bb6e
kuwacom <kuwa.com3@gmail.com>
2024-06-28 12:35:34 +0900

    update: フレーム同期アルゴリズムを書き直し
    debug機能の追加

--- console-video-player.py
+++ console-video-player.py
@@ -1,3 +1,4 @@
+import math
 import cv2
 import shutil
 import time
@@ -9,9 +10,11 @@ def rgbAnsi(r, g, b, txt):
 
 def frameToConsole(
         frame, width=30, height=30,
+        addLinesToBack=0, # デバッグ用の文字を映像の後の行に追加する
         colorMode='color',
         fontColor=[255, 255, 255], # 完全な白だとコンソールソフト側で色が上書きされることがある
         renderMode='line'):
+    height = height - addLinesToBack
     # Resize frame
     frame = cv2.resize(frame, (width, height))
     
@@ -20,10 +23,10 @@ def frameToConsole(
     char = ' '
 
     # Print to console
+    print(f'\033[{height + addLinesToBack}A', end='') # 描画エリアの一番上に戻る
     if colorMode=='color': # フルカラー表示
         if renderMode=='once':
             # まとめて描画
-            print(f'\033[{height}A', end='')
             lines = ''
             for row in frame:
                 line = ''.join(rgbAnsiBg(pixel[2], pixel[1], pixel[0], char) for pixel in row)
@@ -31,7 +34,6 @@ def frameToConsole(
             print(lines, end='')
         else: # line
             # 一行ずつ描画
-            print(f'\033[{height}A', end='')
             for row in frame:
                 line = ''.join(rgbAnsiBg(pixel[2], pixel[1], pixel[0], char) for pixel in row)
                 print(line)
@@ -47,9 +49,7 @@ def frameToConsole(
         gray = (gray / 255) * (len(chars) - 1)
         gray = gray.astype(int)
         
-        # Print to console
         if renderMode=='once':
-            print(f'\033[{height}A', end='')
             lines = ''
             for row in gray:
                 line = ''.join(chars[pixel] for pixel in row)
@@ -58,36 +58,94 @@ def frameToConsole(
                 lines += line + '\n'
             print(lines, end='')
         else:
-            print(f'\033[{height}A', end='')
             for row in gray:
                 line = ''.join(chars[pixel] for pixel in row)
                 if not fontColor == None:
                     line = rgbAnsi(fontColor[0], fontColor[1], fontColor[2], line)
                 print(line)
 
-def videoToConsole(video_path):
-    # Open video file
-    cap = cv2.VideoCapture(video_path)
-    
+def consoleInit():
+    consoleSize = shutil.get_terminal_size()
+    print('\n' * (consoleSize.lines - 1))
+
+def mathFloor(num, fNum):
+    fNum = (10 ** fNum)
+    return math.floor(num * fNum) / fNum
+
+def videoToConsole(videoPath, debug=False):
+    consoleInit()
+    cap = cv2.VideoCapture(videoPath)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frameInterval = mathFloor(1 / fps, 5)
+
+    duration = 0
+    fpsHistory = 0
+
+    frameSkipDelay = 0
+
+    videoStartTime = time.perf_counter()
     while cap.isOpened():
+        frameStartTime = time.perf_counter()
         ret, frame = cap.read()
         if not ret:
             break
+        currentFrameNum = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+
+        addLinesToBack = 0
+        if debug:
+            addLinesToBack = 7
 
         consoleSize = shutil.get_terminal_size()
-
         frameToConsole(
-            frame, width=consoleSize.columns, height=consoleSize.lines-1,
-            colorMode='color',
-            fontColor=None,
+            frame, width=consoleSize.columns, height=consoleSize.lines-1, addLinesToBack=addLinesToBack,
+            colorMode='mono',
+            # fontColor=None,
+            fontColor=[255, 255, 255],
             renderMode='line'
         )
-        # time.sleep(1 / 24)  # frame interval
+        renderEndTime = time.perf_counter()
+        consoleRenderTime = renderEndTime - frameStartTime
 
+        realTime = renderEndTime - videoStartTime
+        frameDelayTime = realTime - duration
+        
+        if duration < realTime - frameInterval:
+            duration += frameInterval
+            skipSleepTime, skipFrameNum = math.modf((frameDelayTime + frameSkipDelay) / frameInterval)
+            
+            skipStartTime = time.perf_counter()
+            # cap.set(cv2.CAP_PROP_POS_FRAMES, currentFrameNum + int(skipFrameNum))
+            for i in range(int(skipFrameNum)):
+                cap.grab()
+                duration += frameInterval
+            skipEndTime = time.perf_counter()
+            frameSkipDelay = skipEndTime - skipStartTime
+
+            while duration > realTime:
+                realTime = time.perf_counter() - videoStartTime
+
+        else:
+            duration += frameInterval
+            while duration > realTime:
+                realTime = time.perf_counter() - videoStartTime
+
+        if debug:
+            endTime = time.perf_counter()
+            fps = mathFloor(1 / (endTime - frameStartTime), 2)
+            fpsHistory += fps
+            print('-'*consoleSize.columns)
+            print('FPSAve: '+str(mathFloor(fpsHistory / currentFrameNum, 2)))
+            print('FPS: '+str(fps))
+            print('Frame Interval: '+str(frameInterval))
+            print('console Render Time: '+str(mathFloor(consoleRenderTime, 6)))
+            print('Duration: '+str(mathFloor(duration, 6))+' / RealTime'+str(mathFloor(realTime, 6)))
+            print('-'*consoleSize.columns)
+
+    videoEndTime = time.perf_counter()
     cap.release()
 
 # Video file path
 videoPath = './video.webm'
 
 # Play video on console
-videoToConsole(videoPath)
+videoToConsole(videoPath, debug=True)
